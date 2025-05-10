2b8d8e9cf2659fc9a20f69825c37a3ef224eb033
jarroffc <jarexe8@gmail.com>
2025-05-11 06:27:03 +0700

    Add files via upload

--- install.sh
+++ install.sh
@@ -112,7 +112,7 @@ install_theme() {
       ;;
   esac
 
-  THEME_URL="https://github.com/SkyzoOffc/Pterodactyl-Theme-Autoinstaller/raw/main/${THEME_NAME}.zip"
+  THEME_URL="https://github.com/jarroffc/jarroffc/raw/main/${THEME_NAME}.zip"
 
   echo -e "${YELLOW}Mengunduh theme $THEME_NAME...${NC}"
   wget -q -O "/root/${THEME_NAME}.zip" "$THEME_URL"
@@ -188,7 +188,7 @@ echo -e "${BLUE}[+] =============================================== [+]${NC}"
 echo -e "                                                                   "
 
 # Unduh file tema
-wget -O /root/stellar.zip https://github.com/SkyzoOffc/Pterodactyl-Theme-Autoinstaller/raw/main/stellar.zip
+wget -O /root/stellar.zip https://github.com/jarroffc/jarroffc/raw/main/stellar.zip
 
 
 # Ekstrak file tema
